1e7f6c277cbf8b2728a34c39e3098c913ed6d79c
Mahendrasinghmanikandan <mahendrasinghmani222@gmail.com>
2023-11-24 11:32:39 +0530

    web authendication codes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../modals/userModal");
 const jwt = require("jsonwebtoken");
+const _ = require("lodash");
 
 const getUser = async (req, res) => {
   try {
@@ -40,4 +41,84 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-module.exports = { getUser, getAllUsers };
+// web
+const careteSignUp = async (req, res) => {
+  try {
+    const result = await User.find({ phoneNumber: req.body.phoneNumber });
+    if (!_.isEmpty(result)) {
+      return res
+        .status(500)
+        .send({ message: "This phone number has already been used." });
+    }
+    await User.create(req.body);
+    return res.status(200).send({ message: "success" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+const getOneUserdata = async (req, res) => {
+  try {
+    const result = await User.find({ phoneNumber: `${req.params.number}` });
+
+    return res.status(200).send({ data: result });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+const makeUserToken = async (req, res) => {
+  try {
+    const result = await User.find({ phoneNumber: `${req.body.number}` });
+    let tokenConstraints = {
+      id: _.get(result, "[0]._id", ""),
+    };
+    const token = jwt.sign(tokenConstraints, process.env.SECRET_KEY, {});
+    await User.findByIdAndUpdate(
+      { _id: _.get(result, "[0]._id", "") },
+      { tokenRef: token }
+    );
+    res.status(200).send({ data: token, message: "Start Your Journey" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+const checkTokenStatus = async (req, res) => {
+  try {
+    let clientdata = {
+      username: _.get(req, "body.userDetails.user", ""),
+    };
+
+    return res.status(200).send({ data: clientdata });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+const makeLogoutUser = async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(
+      { _id: _.get(req, "body.userDetails._id", "") },
+      { tokenRef: "" }
+    );
+    return res.status(200).send({ message: "success" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+module.exports = {
+  getUser,
+  getAllUsers,
+  careteSignUp,
+  getOneUserdata,
+  makeUserToken,
+  checkTokenStatus,
+  makeLogoutUser,
+};
