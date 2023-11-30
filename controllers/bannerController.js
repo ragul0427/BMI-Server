5c0d30e4439091b30d5025fd873a2f10a5cd1478
Mahendrasinghmanikandan <mahendrasinghmani222@gmail.com>
2023-11-30 11:05:44 +0530

    Banner API's  for android apk

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -8,6 +8,7 @@ const createbanner = async (req, res) => {
     return res.status(500).send("Something went wrong while creating banner");
   }
 };
+
 const getbanner = async (req, res) => {
   try {
     const result = await banner.find({});
@@ -16,6 +17,7 @@ const getbanner = async (req, res) => {
     return res.status(500).send("Something went wrong while fetching banner");
   }
 };
+
 const updatebanner = async (req, res) => {
   const { id } = req.params;
   try {
@@ -25,6 +27,7 @@ const updatebanner = async (req, res) => {
     return res.status(500).send("Something went wrong while updating banner");
   }
 };
+
 const deletebanner = async (req, res) => {
   try {
     const { id } = req.params;
@@ -35,4 +38,20 @@ const deletebanner = async (req, res) => {
   }
 };
 
-module.exports = { createbanner, deletebanner, getbanner, updatebanner };
+const getSpecificBanner = async (req, res) => {
+  try {
+    let { id } = req.params;
+    const result = await banner.find({ name: id });
+    return res.status(200).send({ data: result });
+  } catch (err) {
+    return res.status(500).send("Something went wrong");
+  }
+};
+
+module.exports = {
+  createbanner,
+  deletebanner,
+  getbanner,
+  updatebanner,
+  getSpecificBanner,
+};
