cdf89e3e0f8f5c99580dfbdd2fe6443624056dd4
Mahendrasinghmanikandan <mahendrasinghmani222@gmail.com>
2023-12-19 23:25:57 +0530

    file size issue

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors());
 app.use(cookieParser());
